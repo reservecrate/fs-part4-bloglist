1569fc138e06bf0b1bb03f32922fea31364fa002
Aldi Kabulov <reservecrate@gmail.com>
2023-05-06 10:53:16 +0600

    finished part4 exercise 4.20

--- server/controllers/blogs.js
+++ server/controllers/blogs.js
@@ -3,13 +3,6 @@ const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = req => {
-  const authorization = req.get('authorization');
-  return authorization && authorization.startsWith('Bearer ')
-    ? authorization.replace('Bearer ', '')
-    : null;
-};
-
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   res.status(200).json(blogs);
@@ -23,7 +16,7 @@ blogsRouter.get('/:id', async (req, res) => {
 blogsRouter.post('/', async (req, res) => {
   !req.body.likes ? (req.body.likes = 0) : null;
 
-  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
   if (!decodedToken.id) return res.status(401).json({ error: 'invalid token' });
   const user = await User.findById(decodedToken.id);
 
--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,20 +1,18 @@
 const logger = require('./logger');
 
-const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method);
-  logger.info('Path:  ', request.path);
-  logger.info('Body:  ', request.body);
+const requestLogger = (req, res, next) => {
+  logger.info('Method:', req.method);
+  logger.info('Path:  ', req.path);
+  logger.info('Body:  ', req.body);
   logger.info('---');
   next();
 };
 
-const unknownEndpoint = (req, res) => {
+const unknownEndpoint = (req, res) =>
   res.status(404).send({ error: 'unknown endpoint' });
-};
 
 const errorHandler = (err, req, res, next) => {
   logger.error(err.message);
-
   if (err.name === 'CastError') {
     return res
       .status(400)
@@ -24,12 +22,20 @@ const errorHandler = (err, req, res, next) => {
   } else if (err.name === 'JsonWebTokenError') {
     return res.status(400).json({ error: err.message });
   }
-
   next(err);
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    req.token = authorization.replace('Bearer ', '');
+  }
+  next();
 };
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 };
